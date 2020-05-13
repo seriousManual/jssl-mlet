6be7aed732d1f6b65251aecb4b427876e2ec59e5
Manuel Ernst <manuel.ernst@methodpark.de>
2020-05-13 16:20:42 +0200

    dates and durations

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,22 @@
 <script>
-	const message = 'Learn Svelte';
+	import moment from 'moment';
+
+	let date1 = localStorage.getItem('date1');
+	let date2 = null;
+	let duration = '';
+
+	$: localStorage.setItem('date1', date1);
+
+	$: if (date1 && date2) {
+		duration = moment.duration(moment(date1).diff(moment(date2))).humanize();
+	}
 </script>
 
-<style>
-	:global(body) {
-		margin: 0;
-		font-family: Arial, Helvetica, sans-serif;
-	}
-	.App {
-		text-align: center;
-	}
-	.App-header {
-		background-color: #F9F6F6;
-		color: #333;
-		min-height: 100vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		font-size: calc(10px + 2vmin);
-	}
-	.App-link {
-		color: #ff3e00;
-	}
-	.App-logo {
-		height: 40vmin;
-		pointer-events: none;
-		margin-bottom: 1.0rem;
-		animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-	}
-	@keyframes App-logo-spin {
-		from {
-			transform: scale(1);
-		}
-		to {
-			transform: scale(1.06);
-		}
-	}
-</style>
+<div>
+	<input type="date" bind:value={date2}>
+	<input type="date" bind:value={date1}>
 
-<div class="App">
-	<header class="App-header">
-		<img src="/logo.svg" class="App-logo" alt="logo" />
-		<p>
-			Edit <code>src/App.svelte</code> and save to reload.
-		</p>
-		<a
-			class="App-link"
-			href="https://svelte.dev"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			{message}
-		</a>
-	</header>
+	<div>
+		{duration} 
+	</div>
 </div>
