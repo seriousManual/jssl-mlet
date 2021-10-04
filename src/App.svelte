dcdf131ca9c9cbf94927498f3519f5984e3fbfc0
lusiux <tm.github@atlinux.de>
2021-10-04 14:55:36 +0200

    Add possibility to calculate multiple date diffs

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,21 @@
 		margin-top:20px;
 		max-width: 600px;
 	}
+
+	textarea {
+		width: 100%;
+		height: 20rem;
+	}
+
+	button {
+		border: 1px solid black;
+		width: 100%;
+		height: 3rem;
+	}
+
+	h2 {
+		margin-top: 3rem;
+	}
 </style>
 
 <script>
@@ -13,19 +28,32 @@
 	import DateDiffUnits from './DateDiffUnits.svelte';
 
 	let dateOfImage = null;
-	let dateOfBirth = localStorage.getItem('dateOfBirth');;
+	let dateOfBirth = localStorage.getItem('dateOfBirth');
+	let datesText = '';
+	let lines = [];
+	let dates = [];
 
 	$: localStorage.setItem('dateOfBirth', dateOfBirth);
+	$: dates = lines
+
+	function processMultipleDates() {
+		lines = datesText.split("\n").map( date => {
+			return {
+				name: date.split(": ")[0],
+				date: date.split(": ")[1]
+		}})
+	}
 </script>
 
 <div class="root">
 	<div class="form-group">
-		<label>Date of birth</label>
+		<h2>Date of birth</h2>
 		<input type="date" class="form-control" bind:value={dateOfBirth}>
 	</div>
 
+	<h2>Single date</h2>
 	<div class="form-group">
-		<label>Date of image</label>
+
 		<input type="date" class="form-control" bind:value={dateOfImage}>
 	</div>
 
@@ -34,4 +62,17 @@
 	<DateDiff startDate={dateOfBirth} endDate={dateOfImage} unit="weeks"/>
 	<DateDiff startDate={dateOfBirth} endDate={dateOfImage} unit="months"/>
 	<DateDiff startDate={dateOfBirth} endDate={dateOfImage} unit="years"/>
+
+	<h2>Multiple dates</h2>	
+	<div class="form-group">
+		<textarea bind:value="{datesText}" placeholder="title: date"/>
+		<button on:click={processMultipleDates}>Calculate</button>
+	</div>
+
+	{#each dates as date}
+		<h3>{date.name}</h3>
+		<DateDiff startDate={dateOfBirth} endDate={date.date} unit="weeks"/>
+		<DateDiff startDate={dateOfBirth} endDate={date.date} unit="months"/>
+		<DateDiff startDate={dateOfBirth} endDate={date.date} unit="years"/>
+	{/each}
 </div>
